331ce345624180bbad706c2da716606bf8538340
Tulika Jain <tulikajain2006@gmail.com>
2026-07-17 18:23:11 +0530

    Time: 0 ms (100%), Space: 8.7 MB (64.85%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+
+        int n = s.length();
+        vector<int> dp(n + 1, 0);
+
+        dp[0] = 1;
+        dp[1] = (s[0] == '0') ? 0 : 1;
+
+        for (int i = 2; i <= n; i++) {
+
+            int oneDigit = s[i - 1] - '0';
+            int twoDigits = (s[i - 2] - '0') * 10 + (s[i - 1] - '0');
+
+            if (oneDigit >= 1)
+                dp[i] += dp[i - 1];
+
+            if (twoDigits >= 10 && twoDigits <= 26)
+                dp[i] += dp[i - 2];
+        }
+
+        return dp[n];
+    }
+};
